65de409b2cc47bcc42496d55bd4f722e625d20cd
Zignd <zignd.igor@gmail.com>
2020-12-02 00:52:08 -0300

    Add base data and SQL scripts

---
+++ data/import-data.sql
@@ -0,0 +1,3 @@
+COPY users FROM '/opt/data/users.csv' DELIMITER ',' CSV;
+COPY relevance_1 FROM '/opt/data/lista_relevancia_1.csv' DELIMITER ',' CSV;
+COPY relevance_2 FROM '/opt/data/lista_relevancia_2.csv' DELIMITER ',' CSV;
